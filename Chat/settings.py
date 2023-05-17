1181c19a2c9b1bf3eb55f3fbb88b1a2a6e12c0a0
codewitgabi <gabrielmicheal497@gmail.com>
2023-05-17 18:51:37 +0100

    Added env var

--- Chat/settings.py
+++ Chat/settings.py
@@ -140,7 +140,7 @@ CHANNEL_LAYERS = {
 		"BACKEND": "channels_redis.core.RedisChannelLayer",
 		"CONFIG": {
 			"hosts": [
-				("127.0.0.1", 6379)
+				(os.environ.get("REDIS_HOST"))
 			]
 		}
 	}
