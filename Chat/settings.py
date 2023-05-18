727a843e940bea3ab43af2fe837941148803d00a
codewitgabi <gabrielmicheal497@gmail.com>
2023-05-18 17:46:44 +0100

    Updated channel layers config

--- Chat/settings.py
+++ Chat/settings.py
@@ -7,14 +7,14 @@ from .ip_address import get_ip_address
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "kznsh7w6y2bejsksow982bbsbsidneo2826w728e738ensizhsnsksow72y2vsksosowwj"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "chat-group.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://chat-group.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 
 # Application definition
@@ -77,8 +77,14 @@ DATABASES = {
 		"HOST": os.environ.get("PG_HOST"),
 		"PORT": os.environ.get("PG_PORT"),
 	}
-}
+#}
 
+#DATABASES = {
+#	"default": {
+#		'ENGINE': 'django.db.backends.sqlite3',
+#		'NAME': BASE_DIR / 'db.sqlite3'
+#	}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -125,20 +131,23 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
-
+CSRF_TRUSTED_ORIGINS = [
+	"http://localhost",
+	"https://chat-group.up.railway.app",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 CHANNEL_LAYERS = {
-	"default": {
-		"BACKEND": "channels_redis.core.RedisChannelLayer",
-		"CONFIG": {
-			"hosts": [
-				os.environ.get("REDIS_HOST")
-			]
-		}
-	}
+    "default": {
+        "BACKEND": "asgi_redis.RedisChannelLayer",
+        "ROUTING": "widget.routing.channel_routing",
+        "CONFIG": {
+            "hosts": [(os.environ.get("REDIS_HOST"))],
+        },
+    },
 }
