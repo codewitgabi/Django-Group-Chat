d448db012f83429dafaf47c30da83b5e79d197a8
izzyjosh <calebayuba222@gmail.com>
2023-11-20 23:20:14 +0100

    added special character validation

--- main/custom_validators.py
+++ main/custom_validators.py
@@ -3,17 +3,30 @@ from django.utils.translation import gettext_lazy as _
 
 class PasswordCombinationValidator:
     def validate(self,password,user=None):
+        special_char = "@#$_&-+()/?!;:"
+        status = ""
+
         if password.isupper() == True:
-            raise ValidationError(_("password dont have lowercase letters"),
+            raise ValidationError(_("Password don't have lowercase letters"),
                                   code="no lowercase",
                                   )
         if password.islower() == True:
-            raise ValidationError(_("password dont have uppercase letters"),
+            raise ValidationError(_("Password don't have uppercase letters"),
                                   code="no uppercase",
                                   )
         if password.isalpha() ==True:
-            raise ValidationError(_("password should not be letters alone"),
+            raise ValidationError(_("Password should not be letters alone"),
                                   code="no numbers",
                                   )
+        for i in special_char:
+            if password.find(i) != -1:
+                status += "1"
+
+        if status == "":
+            raise ValidationError(_("Password do not contain special characters"),
+                                  code="no special characters",
+                                  )
+
+
     def get_help_text(self):
         return _("the password should be a combination of uppercase,lowercase,number and special symbols")
